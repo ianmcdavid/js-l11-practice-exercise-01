52ee0e4e147ce8d53a4df3ae8438618fdfdc4ddd
ianmcdavid <ianmcdavid25@gmail.com>
2021-05-24 22:30:29 -0700

    Populates a random user generator using Random User Generator API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 const randomFolks = document.querySelector(".random-peeps");
+ 
+const getData = async function() {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    //console.log(data)
+    const usersResults = data.results;
+    //console.log(usersResults);
+    displayUsers(usersResults);
+};
+getData();
+
+const displayUsers = function (usersResults) {
+    randomFolks.innerHTML = "";
+    for (let user of usersResults) {
+        let country = user.location.country;
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+    
+    let userDiv = document.createElement("div");
+    userDiv.innerHTML = `<h3>${name}</h3>
+    <p>${country}</p>
+    <img src=${imageUrl} alt="User avatar" />`;
+    randomFolks.append(userDiv);
+    }
+};
